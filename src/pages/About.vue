ff3478c207dd2c94a4b5599b0da28e0b6d4b1554
Chibuzor Obiora <nbyodai@gmail.com>
2019-08-13 13:59:21 +0100

    Create my about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,8 +2,7 @@
   <Layout>
     <main>
       <header class="max-w-xl md:max-w-2xl xl:max-w-3xl mx-auto text-center px-6 pt-24">
-        <h1 class="text-4xl sm:text-5xl md:text-6xl font-sans font-bold mb-1">About</h1>
-        <p class="text-grey-dark text-lg sm:text-3xl">NbyOdai &hellip;</p>
+        <h1 class="text-4xl sm:text-5xl md:text-6xl font-sans font-bold mb-1">About Me</h1>
       </header>
       <nav class="absolute top-0 left-0 z-20 mt-6 ml-6">
         <g-link
@@ -12,25 +11,36 @@
         >&larr; Home</g-link>
       </nav>
       <article
-        class="max-w-xl md:max-w-2xl xl:max-w-3xl mx-auto px-6 sm:px-12 pt-16 border-b border-gray-300 pb-10 mb-16"
+        class="max-w-xl md:max-w-2xl xl:max-w-3xl mx-auto px-6 sm:px-12 pt-8 border-b border-gray-300 pb-10 mb-16"
       >
         <div class="markdown text-lg leading-normal text-gray-700">
-          <p>This is your about page, say something about you or this blog.</p>
-          <p>
-            You can edit this page in
-            <code class="shiki-inline">src/pages/About.vue</code>
-          </p>
           <hr />
-          <p>Now what are the possibilities of warp drive? Cmdr Riker's nervous system has been invaded by an unknown microorganism. The organisms fuse to the nerve, intertwining at the molecular level. That's why the transporter's biofilters couldn't extract it. The vertex waves show a K-complex corresponding to an REM state. The engineering section's critical. Destruction is imminent. Their robes contain ultritium, highly explosive, virtually undetectable by your transporter.</p>
-          <h2>Communication is not possible</h2>
-          <p>The shuttle has no power. Using the gravitational pull of a star to slingshot back in time? We are going to Starbase Montgomery for Engineering consultations prompted by minor read-out anomalies. Probes have recorded unusual levels of geological activity in all five planetary systems. Assemble a team. Look at records of the Drema quadrant. Would these scans detect artificial transmissions as well as natural signals?</p>
-          <p>Energy discharge in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies. Resistance is futile. Recommend we adjust shield harmonics to the upper EM band when proceeding. These appear to be some kind of power-wave-guide conduits which allow them to work collectively as they perform ship functions. Increase deflector modulation to upper frequency band.</p>
-          <h2>Deflector power at maximum</h2>
-          <p>Unidentified vessel travelling at sub warp speed, bearing 235.7. Fluctuations in energy readings from it, Captain. All transporters off. A strange set-up, but I'd say the graviton generator is depolarized. The dark colourings of the scrapes are the leavings of natural rubber, a type of non-conductive sole used by researchers experimenting with electricity. The molecules must have been partly de-phased by the anyon beam.</p>
-          <p>Sensors indicate human life forms 30 meters below the planet's surface. Stellar flares are increasing in magnitude and frequency. Set course for Rhomboid Dronegar 006, warp seven. There's no evidence of an advanced communication network. Total guidance system failure, with less than 24 hours' reserve power. Shield effectiveness has been reduced 12 percent. We have covered the area in a spherical pattern which a ship without warp drive could cross in the given time.</p>
-          <p>It indicates a synchronic distortion in the areas emanating triolic waves. The cerebellum, the cerebral cortex, the brain stem, the entire nervous system has been depleted of electrochemical energy. Any device like that would produce high levels of triolic waves. These walls have undergone some kind of selective molecular polarization. I haven't determined if our phaser energy can generate a stable field. We could alter the photons with phase discriminators.</p>
-          <p>I have reset the sensors to scan for frequencies outside the usual range. By emitting harmonic vibrations to shatter the lattices. We will monitor and adjust the frequency of the resonators. He has this ability of instantly interpreting and extrapolating any verbal communication he hears. It may be due to the envelope over the structure, causing hydrogen-carbon helix patterns throughout. I'm comparing the molecular integrity of that bubble against our phasers.</p>
-          <p>Resistance is futile.</p>
+          <h2>In a word, maybe two</h2>
+          <img :src="avatar" alt="avatar" />
+          <p class="pt-5">
+            Chibuzor Obiora, along with his Co-founder team at Jara Mobile, joined forces with Carbon (formerly Paylater) to expand loans and access to credit to SMEs.
+            <br />
+            <br />Before that, he worked as a full-stack software developer at Andela, a company building the next crop of technology leaders in Africa. During that spell, he spent 2½ years on the engineering team of TheMuse.com a New York based company, where he worked to make the job search process less sucky.
+            <br />
+            <br />Chibuzor has over five years of core software development and product management experience and has also appeared in numerous prestigious organisations like
+            <a
+              href="https://www.cnet.com/news/how-the-internet-is-changing-africa/"
+              target="_blank"
+            >CNet</a>,
+            <a
+              href="https://www.bbc.co.uk/sounds/play/w3csydd2"
+              target="_blank"
+              rel="noopener noreferrer"
+            >BBC</a>, and
+            <a
+              href="https://www.wsj.com/video/andela-fellow-gives-inside-look-at-startup/DC663956-18BD-4C16-9035-E56E356C303C.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >Wall Street Journal</a> to talk about all things technology.
+            <br />
+            <br />Chibuzor loves dogs, humans and beautiful
+            <em>serverless</em> APIs.
+          </p>
         </div>
       </article>
       <site-footer class="pb-8 sm:pb-10" />
@@ -80,6 +90,9 @@ export default {
   computed: {
     config() {
       return config;
+    },
+    avatar() {
+      return "/images/authors/headshot-1.jpg";
     }
   }
 };
